08117050cac4b33fab64f569dc530004df7cb032
Igor7086 <49655092+Igor7086@users.noreply.github.com>
2019-10-11 20:56:06 +0300

    Less3 task7 @elpiankova

--- Less3_7_1.py
+++ Less3_7_1.py
@@ -2,30 +2,7 @@ a = """We are not what we should be!
 We are not what we need to be.
 But at least we are not what we used to be
  (Football Coach)"""
-"""
-b = a.split()
-c = []
-num = len(b)
-i = 0
 
-for i in range(num):
-    c.append(b[i].strip('!' '(' ')' '"' '-' '_' ',' '.' ':' ';' '/' '?').lower())
-
-c.sort()
-print('количество слов в тексте: ', num)
-print(c)
-
-comp = {}
-co = 1
-for i in range(num-1):
-    if c[i] == c[i+1]:
-        co += 1
-        comp[c[i]] = co
-    else:
-        co = 1
-print(comp)
-
-"""
 
 b = a.split()
 c = []
@@ -33,7 +10,7 @@ num = len(b)
 # i = 0
 
 for i in b:
-    c.append(i.strip('!' '(' ')' '"' '-' '_' ',' '.' ':' ';' '/' '?').lower())
+    c.append(i.strip('!()"-_,.:;?').lower())
 
 c.sort()
 print('количество слов в тексте: ', num)
@@ -41,12 +18,9 @@ print(c)
 
 comp = {}
 co = 1
-for i in range(num-1):
-    if c[i] == c[i+1]:
-        co += 1
-        comp[c[i]] = co
-    else:
-        co = 1
+for i in range(num):
+    comp[c[i]] = c.count(c[i])
+
 print(comp)
 
 
